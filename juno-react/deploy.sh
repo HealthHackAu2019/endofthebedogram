9e04f310a45e6ea05d1a46ea6e286469dde6d280
Daniel Pedroso <daniel.exe@gmail.com>
2019-09-22 11:43:50 +1000

    Fix deployment script

--- juno-react/deploy.sh
+++ juno-react/deploy.sh
@@ -3,4 +3,4 @@
 yarn build
 
 aws s3 sync --acl public-read ./build s3://www.junohealth.com
-aws cloudfront create-invalidation --distribution-id ${DISTRIBUTION_ID} --paths index.html
+aws cloudfront create-invalidation --distribution-id ${DISTRIBUTION_ID} --paths /index.html
